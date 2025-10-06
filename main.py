f48edc6cdd22c412a9cec23a51cec2e3f5eb3d0d
erostho <erostho1202@gmail.com>
2025-10-06 10:14:40 +0700

    Update main.py

--- main.py
+++ main.py
@@ -50,7 +50,6 @@ def _throttle():
 symbols = {
     "Bitcoin": "BTC/USD",
     "Ethereum": "ETH/USD",
-    "BTC/XAU": "BTC/XAU",
     "XAU/USD (Gold)": "XAU/USD",
     "WTI Oil": "CL",
     "EUR/USD": "EUR/USD",
